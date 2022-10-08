419d8b9e1a86c4ad55933555b616423588a32900
KerimHB <kerimhb@live.com>
2022-10-08 14:23:10 -0600

    Update

--- index.py
+++ index.py
@@ -2,38 +2,42 @@ from flask import Flask, render_template, request, session, redirect, url_for, g
 from usuarios import usuarios
 
 
-app=Flask(__name__)
-app.secret_key='123456'
+app = Flask(__name__)
+app.secret_key = '123456'
+
 
 @app.before_request
 def before_request():
     g.usuario = None
     if 'usuario_id' in session:
-        usuario= [x for x in usuarios if x.id == session['usuario_id']][0]
-        g.usuario=usuario
+        usuario = [x for x in usuarios if x.id == session['usuario_id']][0]
+        g.usuario = usuario
 
 
-
-@app.route('/', methods=["POST","GET"])
+@app.route('/', methods=["POST", "GET"])
 def login():
-    if request.method=='POST':
+    if request.method == 'POST':
         session.pop('usuario_id', None)
-        username=request.form['user']
-        password=request.form['pass']
+        username = request.form['user']
+        password = request.form['pass']
 
-        usuario=[x for x in usuarios if x.username == username][0]
-        if usuario and usuario.password == password:
-            session['usuario_id']=usuario.id
-            return redirect(url_for('perfil'))
-        return redirect(url_for('login'))
-    return render_template('login.php')
+        try:
+            usuario = [x for x in usuarios if x.username == username][0]
+            if usuario and usuario.password == password:
+                session['usuario_id'] = usuario.id
+                return redirect(url_for('perfil'))
+            return redirect(url_for('login'))
+        except:
+            print ('Usuario no existe')
+    return render_template('login.html')
 
 
-@app.route('/perfil', methods=["POST","GET"])
+@app.route('/perfil', methods=["POST", "GET"])
 def perfil():
-    if g.usuario==None:
+    if g.usuario == None:
         return redirect(url_for('login'))
-    return render_template('perfil.php')
+    return render_template('perfil.html')
+
 
 @app.route('/logout')
 def logout():
